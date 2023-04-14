e2221548201a244d4c7e4ee07248b84f521e3398
Vampyr7878 <vampyr7878@gmail.com>
2023-04-14 21:52:57 +0200

    1996 and 1997 Megas added with ranged weapon effects.

---
+++ Assets/Scripts/Beast Warriors/BBoom.cs
@@ -0,0 +1,124 @@
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+public class BBoom : BeastWarrior
+{
+    public GameObject rifle;
+
+    public GameObject holster;
+
+    public GameObject hold;
+
+    public GameObject[] lightBarrels;
+
+    public GameObject heavyBarrel;
+
+    public GameObject explosion;
+
+    public GameObject missle;
+
+    public GameObject flash;
+
+    public GameObject ball;
+
+    public Material missleMaterial;
+
+    public Color ballColor;
+
+    private int barrel;
+
+    protected new void FixedUpdate()
+    {
+        base.FixedUpdate();
+        if (lightShoot)
+        {
+            ShootMissle();
+        }
+        if (heavyShoot)
+        {
+            ShootBall();
+        }
+    }
+
+    void ShootMissle()
+    {
+        Vector3 direction = new(-cameraAimHelper.eulerAngles.x, transform.eulerAngles.y, 0f);
+        MeshProjectile(explosion, missle, direction, lightBarrels[barrel], missleMaterial);
+        MeshProjectile(explosion, missle, direction, lightBarrels[barrel + 2], missleMaterial);
+        barrel = barrel == lightBarrels.Length - 3 ? 0 : barrel + 1;
+        lightShoot = false;
+    }
+
+    void ShootBall()
+    {
+        animator.SetTrigger("Shoot");
+        Vector3 direction = new(-cameraAimHelper.eulerAngles.x, transform.eulerAngles.y, 0f);
+        Gradient g = new();
+        GradientColorKey[] colors = new GradientColorKey[2];
+        colors[0].color = ballColor;
+        colors[0].time = 0f;
+        colors[1].color = ballColor;
+        colors[1].time = 1f;
+        GradientAlphaKey[] alphas = new GradientAlphaKey[2];
+        alphas[0].alpha = 1f;
+        alphas[0].time = 0f;
+        alphas[1].alpha = 1f;
+        alphas[1].time = 1f;
+        g.SetKeys(colors, alphas);
+        ParticleProjectile(flash, ball, direction, direction, heavyBarrel, ballColor, ballColor, g);
+        heavyShoot = false;
+    }
+
+    void EquipRifle(GameObject attachment)
+    {
+        rifle.transform.parent = attachment.transform;
+        rifle.transform.localPosition = Vector3.zero;
+        rifle.transform.localEulerAngles = Vector3.zero;
+    }
+
+    public override void OnMeleeWeak(CallbackContext context)
+    {
+        weapon = 1;
+        animator.SetLayerWeight(1, 0f);
+        animator.SetInteger("Weapon", weapon);
+        EquipRifle(holster);
+    }
+
+    public override void OnMeleeStrong(CallbackContext context)
+    {
+        weapon = 2;
+        animator.SetLayerWeight(1, 0f);
+        animator.SetInteger("Weapon", weapon);
+        EquipRifle(holster);
+    }
+
+    public override void OnRangedWeak(CallbackContext context)
+    {
+        weapon = 3;
+        animator.SetLayerWeight(1, 0f);
+        animator.SetInteger("Weapon", weapon);
+        EquipRifle(holster);
+        barrel = 0;
+    }
+
+    public override void OnRangedStrong(CallbackContext context)
+    {
+        weapon = 4;
+        animator.SetLayerWeight(1, 1f);
+        animator.SetInteger("Weapon", weapon);
+        EquipRifle(hold);
+    }
+
+    public override void OnAttack(CallbackContext context)
+    {
+        switch (weapon)
+        {
+            case 3:
+                lightShoot = context.performed;
+                break;
+            case 4:
+                heavyShoot = context.performed;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Beast Warriors/PolarClaw.cs
@@ -0,0 +1,118 @@
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+public class PolarClaw : BeastWarrior
+{
+    public GameObject[] lightBarrels;
+
+    public GameObject[] heavyBarrels;
+
+    public GameObject bullet;
+
+    public GameObject flash;
+
+    public GameObject ball;
+
+    public Color flashColor;
+
+    public Color ballColor;
+
+    public float fireRate;
+
+    public float bulletInaccuracy;
+
+    private float time;
+
+    private int barrel;
+
+    protected new void FixedUpdate()
+    {
+        base.FixedUpdate();
+        if (lightShoot)
+        {
+            if (time >= fireRate)
+            {
+                ShootMachineGun();
+                time = 0;
+            }
+            time += Time.deltaTime;
+        }
+        if (heavyShoot)
+        {
+            ShootBall();
+        }
+    }
+
+    void ShootMachineGun()
+    {
+        int layerMask = 1 << 3;
+        layerMask = ~layerMask;
+        animator.SetTrigger("Shoot");
+        Vector3 direction = new(Random.Range(-bulletInaccuracy, bulletInaccuracy), Random.Range(-bulletInaccuracy, bulletInaccuracy), 1);
+        RaycastBullet(bullet, direction, layerMask, lightBarrels[0]);
+        RaycastBullet(bullet, direction, layerMask, lightBarrels[1]);
+    }
+
+    void ShootBall()
+    {
+        Vector3 direction = new(-cameraAimHelper.eulerAngles.x, transform.eulerAngles.y, 0f);
+        Gradient g = new();
+        GradientColorKey[] colors = new GradientColorKey[2];
+        colors[0].color = flashColor;
+        colors[0].time = 0f;
+        colors[1].color = ballColor;
+        colors[1].time = 1f;
+        GradientAlphaKey[] alphas = new GradientAlphaKey[2];
+        alphas[0].alpha = 1f;
+        alphas[0].time = 0f;
+        alphas[1].alpha = 1f;
+        alphas[1].time = 1f;
+        g.SetKeys(colors, alphas);
+        ParticleProjectile(flash, ball, direction, direction, heavyBarrels[barrel], flashColor, ballColor, g);
+        barrel = barrel == (heavyBarrels.Length - 1) ? 0 : barrel + 1;
+        heavyShoot = false;
+    }
+
+    public override void OnMeleeWeak(CallbackContext context)
+    {
+        weapon = 1;
+        animator.SetLayerWeight(1, 0f);
+        animator.SetInteger("Weapon", weapon);
+    }
+
+    public override void OnMeleeStrong(CallbackContext context)
+    {
+        weapon = 2;
+        animator.SetLayerWeight(1, 0f);
+        animator.SetInteger("Weapon", weapon);
+    }
+
+    public override void OnRangedWeak(CallbackContext context)
+    {
+        weapon = 3;
+        animator.SetLayerWeight(1, 1f);
+        animator.SetInteger("Weapon", weapon);
+    }
+
+    public override void OnRangedStrong(CallbackContext context)
+    {
+        weapon = 4;
+        animator.SetLayerWeight(1, 0f);
+        animator.SetInteger("Weapon", weapon);
+        barrel = 0;
+    }
+
+    public override void OnAttack(CallbackContext context)
+    {
+        switch (weapon)
+        {
+            case 3:
+                lightShoot = context.performed;
+                time = fireRate;
+                break;
+            case 4:
+                heavyShoot = context.performed;
+                break;
+        }
+    }
+}
